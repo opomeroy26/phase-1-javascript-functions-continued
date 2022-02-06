5f976de6acd2cab947521ff1b5ecfb7b3dd4d809
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-06 10:58:10 -0800

    commit tree 407d88930523458735016edc5aaa286e74e6fc9b

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+// // code your solution here
+
+// function saturdayFun(activity = "roller-skate") {
+//     return (`This Saturday, I want to ${activity}!`)
+// }
+
+// function mondayWork(location = "go to the office"){
+//     return (`This Monday, I will ${location}.`)
+
+// }
+
+// function wrapAdjective(symbol) {
+//     return function inner(special) {
+//         return `You are ${symbol}${special}${symbol}!`;
+//     }
+// }
+
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(toDo = "go to the office") {
+    return `This Monday, I will ${toDo}.`;
+}
+
+function wrapAdjective(visualFlair = "*") {
+    return function(adj = "special"){
+        return `You are ${visualFlair}${adj}${visualFlair}!`;
+    }
+
+}
